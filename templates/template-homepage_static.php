833ebd0f241c879c45ea2d3806e8b4236c193d34
karlshaver <karl@meshfresh.com>
2018-08-22 18:05:23 -0400

    Initial landing page

--- templates/template-homepage_static.php
+++ templates/template-homepage_static.php
@@ -15,26 +15,79 @@ and <span class="orange">800 women a day</span> die due to pregnancy or childbir
 		<p class="heading6">Leaern About the People and Programs We Support</p>
 		<img src="<?php echo get_template_directory_uri(); ?>/img/map1.png" alt="">
 	</div>
-	<!-- <div class="container">
-		<div class="row">
-			<div class="columns-9">
-				<//?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
-
-					<h1><//?php the_title(); ?></h1>
-
-					<//?php the_content(); ?>
-
-				<//?php endwhile; ?>
+	<div class="panel text-grid">
+		<div class="container">
+			<div class="row">
+				<div class="columns-10 offset-by-1">
+					<div class="columns-4">
+						<div class="grid-item">
+							<p class="heading6">Learn</p>
+							<p class="desc">Watch our latest film—Giving Birth in America: Louisiana</p>
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+								<style type="text/css">
+									.st0{fill:#EED9BD;}
+									.st1{fill:#EC742E;}
+								</style>
+								<polygon class="st1" points="71.9,50.7 71.9,50.7 65.6,44.4 65.6,44.4 34.1,12.9 28.3,18.8 59.7,50.2 28.1,81.8 34.4,88.2
+									39.3,83.3 66,56.5 71.9,50.7 "/>
+							</svg>
+						</div>
+					</div>
+					<div class="columns-4">
+						<div class="grid-item">
+							<p class="heading6">Engage</p>
+							<p class="desc">Meet our community of engaged supporters</p>
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+								<style type="text/css">
+									.st0{fill:#EED9BD;}
+									.st1{fill:#EC742E;}
+								</style>
+								<polygon class="st1" points="71.9,50.7 71.9,50.7 65.6,44.4 65.6,44.4 34.1,12.9 28.3,18.8 59.7,50.2 28.1,81.8 34.4,88.2
+									39.3,83.3 66,56.5 71.9,50.7 "/>
+							</svg>
+						</div>
+					</div>
+					<div class="columns-4">
+						<div class="grid-item">
+							<p class="heading6">Grants</p>
+							<p class="desc">Donate $x could support a solar suitcase for a clinic in Tanzania</p>
+							<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+								 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+								<style type="text/css">
+									.st0{fill:#EED9BD;}
+									.st1{fill:#EC742E;}
+								</style>
+								<polygon class="st1" points="71.9,50.7 71.9,50.7 65.6,44.4 65.6,44.4 34.1,12.9 28.3,18.8 59.7,50.2 28.1,81.8 34.4,88.2
+									39.3,83.3 66,56.5 71.9,50.7 "/>
+							</svg>
+						</div>
+					</div>
+				</div>
 			</div>
-
-			<div class="columns-3">
-
-
-				<//?php get_sidebar(); ?>
-			</div>
-
 		</div>
-	</div> -->
+	</div>
+	<div class="panel feature">
+		<div class="container">
+			<div class="row">
+				<div class=" columns-3 offset-by-1 intro">
+					<p class="title">Their Stories</p>
+					<p class="desc">In 2017, the HOPE Foundation was also awareded an emergency grant of $35,000 to care for Rohingya refugees. Photographer Josh Estey traveled there to help tell the story of this humanitarian crisis.</p>
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
+						<style type="text/css">
+							.st0{fill:#EED9BD;}
+							.st1{fill:#EC742E;}
+						</style>
+						<polygon class="st1" points="71.9,50.7 71.9,50.7 65.6,44.4 65.6,44.4 34.1,12.9 28.3,18.8 59.7,50.2 28.1,81.8 34.4,88.2
+							39.3,83.3 66,56.5 71.9,50.7 "/>
+					</svg>
+				</div>
+			</div>
+		</div>
+		<img class="feature-image" src="<?php echo get_template_directory_uri(); ?>/img/5K3A5709.png" alt="">
+	</div>
 
 </main><!-- End of Content -->
 
