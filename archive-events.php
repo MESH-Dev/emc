fa893e0b97546e66dff55d3a4ac22de4dfe48b03
karlshaver <karl@meshfresh.com>
2018-09-18 19:06:27 -0400

    Initial horizontal scroll js

--- archive-events.php
+++ archive-events.php
@@ -113,7 +113,7 @@
                            <?php } ?>
 
                   </ul>
-						<svg class="scroll" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+						<svg class="scroll topic-scroll" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 							 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
 							<style type="text/css">
 								.st0{fill:#EED9BD;}
--- js/mesh.js
+++ js/mesh.js
@@ -977,4 +977,85 @@ function loadFilms (filmTopic, query) { //*
 //     });
 //   });
 
+// duration of scroll animation
+var scrollDuration = 300;
+// paddles
+// var leftPaddle = document.getElementsByClassName('left-paddle');
+var rightPaddle = document.getElementsByClassName('topic-scroll');
+console.log(rightPaddle);
+// get items dimensions
+var itemsLength = $('.filter-bar ul li').length;
+var itemSize = $('.filter-bar ul li').outerWidth(true);
+// get some relevant size for the paddle triggering point
+var paddleMargin = 20;
+
+// get wrapper width
+var getMenuWrapperSize = function() {
+	return $('.e-topic-filters').outerWidth();
+}
+var menuWrapperSize = getMenuWrapperSize();
+// the wrapper is responsive
+$(window).on('resize', function() {
+	menuWrapperSize = getMenuWrapperSize();
+});
+// size of the visible part of the menu is equal as the wrapper size
+var menuVisibleSize = menuWrapperSize;
+
+// get total width of all menu items
+var getMenuSize = function() {
+	return itemsLength * itemSize;
+};
+var menuSize = getMenuSize();
+// get how much of menu is invisible
+var menuInvisibleSize = menuSize - menuWrapperSize;
+
+// get how much have we scrolled to the left
+var getMenuPosition = function() {
+	return $('.e-topic-filters').scrollLeft();
+};
+
+// finally, what happens when we are actually scrolling the menu
+$('.e-topic-filters').on('scroll', function() {
+
+	// get how much of menu is invisible
+	menuInvisibleSize = menuSize - menuWrapperSize;
+	// get how much have we scrolled so far
+	var menuPosition = getMenuPosition();
+
+	var menuEndOffset = menuInvisibleSize - paddleMargin;
+
+	// show & hide the paddles
+	// depending on scroll position
+// 	if (menuPosition <= paddleMargin) {
+// 		$(leftPaddle).addClass('hidden');
+// 		$(rightPaddle).removeClass('hidden');
+// 	} else if (menuPosition < menuEndOffset) {
+// 		// show both paddles in the middle
+// 		$(leftPaddle).removeClass('hidden');
+// 		$(rightPaddle).removeClass('hidden');
+// 	} else if (menuPosition >= menuEndOffset) {
+// 		$(leftPaddle).removeClass('hidden');
+// 		$(rightPaddle).addClass('hidden');
+// }
+
+	// // print important values
+	// $('#print-wrapper-size span').text(menuWrapperSize);
+	// $('#print-menu-size span').text(menuSize);
+	// $('#print-menu-invisible-size span').text(menuInvisibleSize);
+	// $('#print-menu-position span').text(menuPosition);
+
+});
+
+console.log(menuInvisibleSize);
+
+// scroll to left
+$(rightPaddle).on('click', function() {
+	$('.e-topic-filters').animate( { scrollLeft: menuInvisibleSize}, scrollDuration);
+});
+
+// scroll to right
+// $(leftPaddle).on('click', function() {
+// 	$('.menu').animate( { scrollLeft: '0' }, scrollDuration);
+// });
+
 });
