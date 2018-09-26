d6d82d8548a33d60a267de53b9d098da44cd2a48
Shaun <shaun@meshfresh.com>
2018-09-26 16:21:33 -0400

    Add WP styling for content areas

--- page.php
+++ page.php
@@ -1,6 +1,33 @@
 <?php get_header(); ?>
 
-<?php echo get_template_part('/partials/short-banner-no-video'); ?>
+ <?php
+   $background_img = get_field('background_image');
+   $background_image_url = $background_img['sizes']['short-banner'];
+   $v_ogg = get_field('video_ogg');
+   $vo_url = $v_ogg['url'];
+   $v_mp4 = get_field('video_mp4');
+   $vm_url = $v_mp4['url'];
+   $v_webm = get_field('video_webm');
+   $vw_url = $v_webm['url'];
+   $l_page_callout = get_field('banner_callout_text');
+?>
+<div class="welcome-gate interior">
+   <!-- <img src="<//?php echo get_template_directory_uri(); ?>/img/everymothercounts_logo_primary_white_40in.png" alt=""> -->
+   <?php if ($vm_url == '' && $vo_url == '' && $vw_url == ""){ ?>
+   <div class="welcome-gate-bg" style="background-image:url('<?php echo $background_image_url; ?>');"></div>
+   <?php } ?>
+   <div class="banner-text columns-5 offset-by-1">
+      <p class="top-callout"><?php the_title(); ?></p>
+      <h1 class="page-title heading1"><?php echo $l_page_callout; ?></h1>
+   </div>
+   <?php if ($vm_url != '' && $vo_url != '' && $vw_url != ""){ ?>
+      <video placeholder="<?php echo $background_image_url; ?>" autoplay="true" loop="true" muted="true">
+         <source src ="<?php echo $vm_url; ?>" autoplay="true" loop="true" muted="true">
+         <source src ="<?php echo $vo_url; ?>" autoplay="true" loop="true" muted="true">
+         <source src ="<?php echo $vw_url; ?>" autoplay="true" loop="true" muted="true">
+      <video>
+   <?php } ?>
+</div>
 <main id="content" class="text">
 
 	<div class="container">
