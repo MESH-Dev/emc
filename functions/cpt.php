6f8524c3fd30f6fc752062fdba0485bb8cb1748d
Shaun <shaun@meshfresh.com>
2018-10-23 09:53:54 -0400

    Remove Location taxonomy from Community Members posts

--- functions/cpt.php
+++ functions/cpt.php
@@ -491,43 +491,43 @@ function community_post_type() {
 add_action( 'init', 'community_post_type', 0 );
 
 // Register Custom Taxonomy
-function community_member_location_taxonomy() {
+// function community_member_location_taxonomy() {
 
-	$labels = array(
-		'name'                       => _x( 'Community Member Locations', 'Taxonomy General Name', 'text_domain' ),
-		'singular_name'              => _x( 'Community Member Location', 'Taxonomy Singular Name', 'text_domain' ),
-		'menu_name'                  => __( 'Community Member Location', 'text_domain' ),
-		'all_items'                  => __( 'All Locations', 'text_domain' ),
-		'parent_item'                => __( 'Parent Item', 'text_domain' ),
-		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
-		'new_item_name'              => __( 'New Location', 'text_domain' ),
-		'add_new_item'               => __( 'Add New Location', 'text_domain' ),
-		'edit_item'                  => __( 'Edit Location', 'text_domain' ),
-		'update_item'                => __( 'Update Location', 'text_domain' ),
-		'view_item'                  => __( 'View Location', 'text_domain' ),
-		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
-		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
-		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
-		'popular_items'              => __( 'Popular Items', 'text_domain' ),
-		'search_items'               => __( 'Search Topic', 'text_domain' ),
-		'not_found'                  => __( 'Not Found', 'text_domain' ),
-		'no_terms'                   => __( 'No items', 'text_domain' ),
-		'items_list'                 => __( 'Items list', 'text_domain' ),
-		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
-	);
-	$args = array(
-		'labels'                     => $labels,
-		'hierarchical'               => true,
-		'public'                     => true,
-		'show_ui'                    => true,
-		'show_admin_column'          => true,
-		'show_in_nav_menus'          => true,
-		'show_tagcloud'              => true,
-		'show_in_rest'               => true,
-	);
-	register_taxonomy( 'member_location', array( 'community' ), $args );
+// 	$labels = array(
+// 		'name'                       => _x( 'Community Member Locations', 'Taxonomy General Name', 'text_domain' ),
+// 		'singular_name'              => _x( 'Community Member Location', 'Taxonomy Singular Name', 'text_domain' ),
+// 		'menu_name'                  => __( 'Community Member Location', 'text_domain' ),
+// 		'all_items'                  => __( 'All Locations', 'text_domain' ),
+// 		'parent_item'                => __( 'Parent Item', 'text_domain' ),
+// 		'parent_item_colon'          => __( 'Parent Item:', 'text_domain' ),
+// 		'new_item_name'              => __( 'New Location', 'text_domain' ),
+// 		'add_new_item'               => __( 'Add New Location', 'text_domain' ),
+// 		'edit_item'                  => __( 'Edit Location', 'text_domain' ),
+// 		'update_item'                => __( 'Update Location', 'text_domain' ),
+// 		'view_item'                  => __( 'View Location', 'text_domain' ),
+// 		'separate_items_with_commas' => __( 'Separate items with commas', 'text_domain' ),
+// 		'add_or_remove_items'        => __( 'Add or remove items', 'text_domain' ),
+// 		'choose_from_most_used'      => __( 'Choose from the most used', 'text_domain' ),
+// 		'popular_items'              => __( 'Popular Items', 'text_domain' ),
+// 		'search_items'               => __( 'Search Topic', 'text_domain' ),
+// 		'not_found'                  => __( 'Not Found', 'text_domain' ),
+// 		'no_terms'                   => __( 'No items', 'text_domain' ),
+// 		'items_list'                 => __( 'Items list', 'text_domain' ),
+// 		'items_list_navigation'      => __( 'Items list navigation', 'text_domain' ),
+// 	);
+// 	$args = array(
+// 		'labels'                     => $labels,
+// 		'hierarchical'               => true,
+// 		'public'                     => true,
+// 		'show_ui'                    => true,
+// 		'show_admin_column'          => true,
+// 		'show_in_nav_menus'          => true,
+// 		'show_tagcloud'              => true,
+// 		'show_in_rest'               => true,
+// 	);
+// 	register_taxonomy( 'member_location', array( 'community' ), $args );
 
-}
-add_action( 'init', 'community_member_location_taxonomy', 0 );
+// }
+// add_action( 'init', 'community_member_location_taxonomy', 0 );
 
 ?>
