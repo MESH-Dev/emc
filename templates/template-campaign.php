d24f2f0fbc135c03aa3c3dd93ace1966784533f1
Shaun <shaun@meshfresh.com>
2018-10-24 14:54:48 -0400

    Updates to image sections, new popup section

--- templates/template-campaign.php
+++ templates/template-campaign.php
@@ -457,7 +457,7 @@
 		</div>
 	</div>
 <?php }elseif($panel_type == 'cta-grid'){?>
-<div class="panel text-grid interior" ><!-- style="background-color:#97a88f;" -->
+<div class="panel text-grid interior" style="background-color:#97a88f;"><!-- style="background-color:#97a88f;" -->
 		<div class="container">
 			<div class="row">
 				<?php 
@@ -524,7 +524,7 @@
 								<p class="heading2"><?php echo $cta_title; ?></p>
 								
 								<?php if ($cta_link != ''){?>
-									</a>
+									
 									<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 									 viewBox="0 0 100 100" style="enable-background:new 0 0 100 100;" xml:space="preserve">
 									<style type="text/css">
@@ -534,6 +534,7 @@
 									<polygon class="st1" points="71.9,50.7 71.9,50.7 65.6,44.4 65.6,44.4 34.1,12.9 28.3,18.8 59.7,50.2 28.1,81.8 34.4,88.2
 										39.3,83.3 66,56.5 71.9,50.7 "/>
 								</svg>
+								</a>
 								<?php } ?>
 								<?php if ($cta_text != ''){ ?>
 								<p class="desc heading4"><?php echo $cta_text; ?></p>
